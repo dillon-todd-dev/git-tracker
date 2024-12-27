b52baa598878e7d79ad382e40d007a46fae0c7c4
dillon-todd-dev <dillontodd.dev@gmail.com>
2024-12-27 07:11:42 -0600

    add basic unstyled layout and setup github integration

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,49 @@
 'use client';
 
 import useProjects from '@/hooks/use-project';
-import { useUser } from '@clerk/nextjs';
+import { ExternalLink, Github } from 'lucide-react';
+import Link from 'next/link';
 
 const DashboardPage = () => {
   const { project } = useProjects();
 
-  return <div>{project?.name}</div>;
+  return (
+    <div>
+      <div className='flex flex-wrap items-center justify-between gap-y-4'>
+        {/* github link */}
+        <div className='w-fit rounded-md bg-primary px-4 py-3'>
+          <div className='flex items-center'>
+            <Github className='size-5 text-white' />
+            <div className='ml-2'>
+              <p className='text-sm font-medium text-white'>
+                This project is linked to{' '}
+                <Link
+                  href={project?.githubUrl ?? ''}
+                  className='inline-flex items-center text-white/80 hover:underline'
+                >
+                  {project?.githubUrl}
+                  <ExternalLink className='ml-1 size-4' />
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className='h-4'></div>
+
+        <div className='flex items-center gap-4'>
+          TeamMembers InviteButton ArchiveButton
+        </div>
+      </div>
+
+      <div className='mt-4'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-5'>
+          AskQuestionCard MeetingCard
+        </div>
+      </div>
+
+      <div className='mt-8'>CommitLog</div>
+    </div>
+  );
 };
 export default DashboardPage;
